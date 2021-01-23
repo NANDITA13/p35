31ed8dede25c18b4dfef49f18955bc297f8b97e2
NANDITA13 <70268353+NANDITA13@users.noreply.github.com>
2021-01-23 11:15:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var dog, dogImg, happyDog, happyDogImg;
+
+var database, foods, foodStock;
+
+
+function preload() {
+
+  dogImg = loadImage("images/dogImg.png");
+  
+  happyDogIme = loadImage("images/dogImg1.png");
+
+}
+
+function setup() {
+
+  createCanvas(500, 500);
+  
+  dog=createSprite(250,250,5,5);
+	dog.addImage(dogImg);
+  dog.scale=0.2
+
+  
+
+  database=firebase.database ();
+  
+  var foodRef = database.ref ("food");
+  foodRef.on ("value",readStock);
+  
+}
+
+
+function draw() { 
+  
+  background(46, 139, 87);
+
+  function keyWentDown(UP_ARROW) {
+
+    writeStock(foods);
+
+    dog.addImage(happyDog);
+
+   }
+
+   drawSprites();
+
+   textSize(20);
+   fill("blue");
+
+   text("Press UP_ARROW Key to feed Drago Milk!",80,20)
+
+}
+
+ 
+
+  
+
+  function readStock (data) {
+
+    foods=data.val();
+
+  }
+
+  function writeStock(x) {
+
+    if(x<=0) {
+      x=0;
+    }
+
+    else {
+      x=x-1;
+    }
+
+    database.ref('/').update({
+      food:x
+    })
+    
+  }
+  
+
+
+
+
+
